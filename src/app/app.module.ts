82d2204846adcf6cc879ebb4d4378ca7a4a743d2
Zohidbek Mengliboyev <uzrivojcoder@gmail.com>
2023-03-07 16:51:39 +0500

    Interact with the OpenWeather API over HTTP to get weather data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '0f7bcd6e90cb34e666b8e8676789eadf'
 };
