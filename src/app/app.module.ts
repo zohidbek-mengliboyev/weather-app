9ed66e098dd3b9a63ff9c9f62e89f829dbe6924c
zohidbek-mengliboyev <uzrivojcoder@gmail.com>
2023-03-09 00:46:20 +0500

    Staying up to date with in-app notifications and Deploying our app with Firebase hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HeaderComponent } from './header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -23,14 +38,29 @@ import { environment } from '../environments/environment';
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'weather-app-bf82f',
+    appId: '1:36365989459:web:45421a656d3f22ce5d78a6',
+    databaseURL: 'https://weather-app-bf82f-default-rtdb.firebaseio.com',
+    storageBucket: 'weather-app-bf82f.appspot.com',
+    apiKey: 'AIzaSyBgmo3JkTF9znI3e8NR-PS9Jn5b2DXJw6k',
+    authDomain: 'weather-app-bf82f.firebaseapp.com',
+    messagingSenderId: '36365989459',
+  },
   production: true,
   apiUrl: 'https://api.openweathermap.org/data/2.5/',
   apiKey: '0f7bcd6e90cb34e666b8e8676789eadf'
